1a73f1574279fc361ba2021e67178df68ca7566e
vraajb <vraajb@amgen.com>
2019-09-12 12:24:27 +0530

    Base Commit for Sitecore with SXA

---
+++ Code/Sitecore.Foundation.MultiSiteAliases.9.1.1/Model/AliasInfo.cs
@@ -0,0 +1,145 @@
+﻿namespace Sitecore.Foundation.MultiSiteAliases.Model
+{
+    using Microsoft.Extensions.DependencyInjection;
+    using Sitecore;
+    using Sitecore.Abstractions;
+    using Sitecore.Data;
+    using Sitecore.Data.Fields;
+    using Sitecore.Data.Items;
+    using Sitecore.DependencyInjection;
+    using Sitecore.Diagnostics;
+    using Sitecore.Text;
+    using Sitecore.Web;
+    using Sitecore.XA.Foundation.Multisite;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    internal class AliasInfo
+    {
+        public AliasInfo() : base()
+        {
+
+        }
+        public readonly ListString path;
+        public AliasInfo(string value)
+        {
+            Assert.ArgumentNotNullOrEmpty(value, nameof(value));
+            value = StringUtil.RemovePrefix(MultiSiteAliases.Constants.ForwardSlash, value);
+            value = StringUtil.RemovePostfix(MultiSiteAliases.Constants.ForwardSlash, value);
+            this.path = new ListString(value, MultiSiteAliases.Constants.ForwardSlashChar);
+        }
+        public IEnumerable<string> Ascenders
+        {
+            get
+            {
+                if (this.path.Count > 1)
+                {
+                    for (int i = 0; i < this.path.Count - 1; ++i)
+                        yield return this.path[i];
+                }
+            }
+        }
+        public IEnumerable<string> AscendersAndName
+        {
+            get
+            {
+                return (IEnumerable<string>)this.path.Items;
+            }
+        }
+        public string AliasesNameWithPath
+        {
+            get
+            {
+                return string.Join(MultiSiteAliases.Constants.ForwardSlash, AscendersAndName);
+            }
+        }
+        public string Name
+        {
+            get
+            {
+                return this.path[this.path.Count - 1];
+            }
+        }
+        private List<Sites> Sites()
+        {
+            var service = ServiceLocator.ServiceProvider.GetRequiredService<ISiteInfoResolver>();
+            List<Sites> sitesInfo = new List<Sites>();
+
+            foreach (var site in service.Sites)
+            {
+                GetSiteInfo(sitesInfo, 
+                            site.Name, 
+                            site?.Properties[MultiSiteAliases.Constants.SiteProperties.SiteLevelAliases],
+                            site?.Properties[MultiSiteAliases.Constants.SiteProperties.RootPath]);
+            }
+            foreach (var site in Sitecore.Sites.SiteManager.GetSites())
+            {
+                GetSiteInfo(sitesInfo, 
+                            site.Name, 
+                            site?.Properties[MultiSiteAliases.Constants.SiteProperties.SiteLevelAliases],
+                            site?.Properties[MultiSiteAliases.Constants.SiteProperties.RootPath]);
+            }
+            return sitesInfo;
+        }
+
+
+
+        private static void GetSiteInfo(List<Sites> sitesInfo, string siteName, string aliasesVal, string rootPath)
+        {
+            if (string.IsNullOrEmpty(siteName) || string.IsNullOrEmpty(rootPath) || string.IsNullOrEmpty(aliasesVal) || sitesInfo.Any(s => s.SiteName == siteName))
+                return;
+            if (ID.TryParse(aliasesVal, out ID aliasesId) && !aliasesId.IsNull && aliasesId.Guid != Guid.Empty)
+                sitesInfo.Add(new Sites() { SiteName = siteName, AliasesPathId = aliasesId, RootPath = rootPath });
+        }
+
+        public CustomAliases AliasesWindow(Item item)
+        {
+            var customAliases = new CustomAliases();
+            customAliases.ItemId = item.ID;
+            var selectedItemFullPath = item.Paths.FullPath;
+
+            foreach (var site in Sites())
+            {
+                if (selectedItemFullPath.StartsWith(site.RootPath) || selectedItemFullPath.StartsWith(MultiSiteAliases.Constants.MediaPath))
+                    ProcessEachSite(item, customAliases, site.SiteName, site.AliasesPathId);
+            }
+            
+
+            return customAliases;
+        }
+
+        private static void ProcessEachSite(Item item, CustomAliases customAliases, string siteName, ID id)
+        {
+            var customAliasesFolderItem = Context.ContentDatabase.GetItem(id);
+            if (customAliasesFolderItem != null)
+            {
+                //To Show in Multi List for user
+                customAliases.AllSites.Add(new Sites() { SiteName = siteName, AliasesPathId = id });
+
+                //To Show Aliases as same Url pattern
+                var roothFullPath = customAliasesFolderItem.Paths.FullPath + MultiSiteAliases.Constants.ForwardSlash;
+
+                var itemId = item.ID;
+                foreach (var eachAliases in customAliasesFolderItem
+                                            .Axes.GetDescendants()
+                                            .Where(f => f.TemplateID == MultiSiteAliases.Constants.Template.AliasesTemplateId))
+                {
+                    LinkField lnkField = eachAliases.Fields[MultiSiteAliases.Constants.Fields.LinkedField];
+                    if (lnkField?.TargetID == itemId)
+                    {
+                        var path = eachAliases.Paths.FullPath.Replace(roothFullPath, string.Empty);
+                        //To Show as already Exists Entry
+                        customAliases.AddedSites.Add(new Sites()
+                        {
+                            SiteName = siteName,
+                            AliasesPathId = id,
+                            AliasesName = path,
+                            AliasesItemId = eachAliases.ID
+                        });
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Code/Sitecore.Foundation.MultiSiteAliases.9.1.1/Pipeline/AliasResolver.cs
@@ -0,0 +1,116 @@
+﻿namespace Sitecore.Foundation.MultiSiteAliases.Pipeline
+{
+    using Microsoft.Extensions.DependencyInjection;
+    using Sitecore;
+    using Sitecore.Abstractions;
+    using Sitecore.Configuration;
+    using Sitecore.Data;
+    using Sitecore.Data.Items;
+    using Sitecore.DependencyInjection;
+    using Sitecore.Diagnostics;
+    using Sitecore.Pipelines.HttpRequest;
+    using Sitecore.Pipelines.HttpRequest.HandlerMapping;
+    using Sitecore.Web;
+    public class AliasResolver : HttpRequestProcessor
+    {
+
+        public AliasResolver()
+        { }
+        private string AliasItemPath { get; set; }
+        public override void Process(HttpRequestArgs args)
+        {
+            Assert.ArgumentNotNull(args, MultiSiteAliases.Constants.Args);
+            if (!Settings.AliasesActive)
+            {
+                Tracer.Warning(MultiSiteAliases.Constants.AliasesNotActive);
+                return;
+            }
+            Database database = Context.Database;
+            if (database == null)
+            {
+                Tracer.Warning(MultiSiteAliases.Constants.ContextDatabaseNotFound);
+                return;
+            }
+
+            Profiler.StartOperation(MultiSiteAliases.Constants.ResolveAliases);
+
+            if (ID.TryParse(Context.Site?.Properties[MultiSiteAliases.Constants.SiteProperties.SiteLevelAliases], out ID aliasesPathId))
+            {
+                var aliasesPathItem = database.GetItem(aliasesPathId);
+                if (aliasesPathItem == null)
+                    return;
+
+                var aliaseFullPath = aliasesPathItem?.Paths?.FullPath;
+                if (aliaseFullPath == null || !aliaseFullPath.StartsWith(MultiSiteAliases.Constants.AliasesRootPath))
+                    return;
+
+
+                AliasItemPath = aliaseFullPath.Replace(MultiSiteAliases.Constants.AliasesRootPath, "") + '/' + args.LocalPath;
+                if (database.Aliases.Exists(AliasItemPath))
+                {
+                    if (this.ProcessItem(args))
+                    {
+                        Item obj = Context.Item;
+                        if (obj != null && obj.Paths != null && obj.Paths.IsMediaItem && obj.TemplateID != TemplateIDs.MediaFolder)
+                        {
+                            var mediaManager = ServiceLocator.ServiceProvider.GetRequiredService<BaseMediaManager>();
+                            string mediaUrl = mediaManager?.GetMediaUrl((MediaItem)obj);
+                            if (!string.IsNullOrEmpty(mediaUrl))
+                            {
+                                var handlerMapper = (HandlerMapper)new DirectHandlerMapper(ServiceLocator.ServiceProvider.GetRequiredService<BaseFactory>()?.GetCustomHandlers());
+                                if (handlerMapper != null && handlerMapper.TryFindMatchingHandler(mediaUrl, out CustomHandler matchingHandler) && matchingHandler != null)
+                                {
+                                    Context.Data.RawUrl = mediaUrl;
+                                    args.HttpContext.RewritePath(matchingHandler.Handler, mediaUrl, args.Url.QueryString, true);
+                                    args.AbortPipeline();
+                                }
+                            }
+                        }
+                    }
+                    //To Redirect External Site
+                    else if (this.ProcessExternalUrl(args))
+                    {
+                        string path = Context.Page?.FilePath;
+                        if (!string.IsNullOrEmpty(path) && WebUtil.IsExternalUrl(path))
+                            WebUtil.Redirect(path);
+                    }
+
+                }
+            }
+            Profiler.EndOperation();
+        }
+
+        private bool ProcessExternalUrl(HttpRequestArgs args)
+        {
+            string targetUrl = Context.Database.Aliases.GetTargetUrl(AliasItemPath);
+            if (targetUrl.Length > 0)
+                return this.ProcessExternalUrl(targetUrl);
+            return false;
+        }
+        private bool ProcessExternalUrl(string path)
+        {
+            if (Context.Page.FilePath.Length > 0)
+                return false;
+            Context.Page.FilePath = path;
+            return true;
+        }
+
+        private bool ProcessItem(HttpRequestArgs args)
+        {
+            // Use the AliasItemPath property to find the site specific aliases
+            ID targetID = Context.Database.Aliases.GetTargetID(AliasItemPath);
+            if (!targetID.IsNull)
+            {
+                Item item = args.GetItem(targetID);
+                if (item != null && Context.Item == null)
+                {
+                    Context.Item = item;
+                }
+                return true;
+            }
+            // Use the AliasItemPath property to output porper information
+            Tracer.Error(string.Format(MultiSiteAliases.Constants.AliasesNotLinked, AliasItemPath));
+            return false;
+        }
+    }
+}
